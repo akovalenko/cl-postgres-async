6234b5f61ad9f2efd88038f11fba1a057dad5045
Anton Kovalenko <anton@sw4me.com>
2015-02-16 02:29:42 +0300

    Copy s-sql and simple-date, modified postgres glue

---
+++ simple-date.asd
@@ -0,0 +1,30 @@
+(defpackage :simple-date-system
+  (:use :common-lisp :asdf))
+(in-package :simple-date-system)
+
+(defsystem :simple-date
+  :components 
+  ((:module :simple-date
+            :components ((:file "simple-date")))))
+
+(defsystem :simple-date-postgres-async-glue
+  :depends-on (:simple-date :cl-postgres)
+  :components
+  ((:module :simple-date
+            :components
+            ((:file "cl-postgres-glue")))))
+
+(defsystem :simple-date-tests
+  :depends-on (:eos :simple-date)
+  :components
+  ((:module :simple-date
+            :components ((:file "tests")))))
+
+(defmethod perform ((op asdf:test-op) (system (eql (find-system :simple-date))))
+  (asdf:oos 'asdf:load-op :simple-date-tests)
+  (funcall (intern (string :run!) (string :Eos)) :simple-date))
+
+(defmethod perform :after ((op asdf:load-op) (system (eql (find-system :simple-date))))
+  (when (and (find-package :cl-postgres-async)
+             (not (find-symbol (symbol-name '#:+apomo-postgres-day-offset+) :simple-date)))
+    (asdf:oos 'asdf:load-op :simple-date-postgres-async-glue)))
---
+++ simple-date/cl-postgres-glue.lisp
@@ -0,0 +1,42 @@
+(in-package :simple-date)
+
+;; Postgresql days are measured from 01-01-2000, whereas simple-date
+;; uses 01-03-2000.
+
+(defconstant +apomo-postgres-day-offset+ -60)
+(defconstant +apomo-usecs-in-one-day+ (* 1000 1000 3600 24))
+
+(flet ((interpret-timestamp (usecs)
+         (multiple-value-bind (days usecs) (floor usecs +apomo-usecs-in-one-day+)
+           (make-instance 'timestamp :days (+ days +apomo-postgres-day-offset+)
+                          :ms (floor usecs 1000)))))
+  (cl-postgres:set-sql-datetime-readers
+   :date (lambda (days)
+           (make-instance 'date :days (+ days +apomo-postgres-day-offset+)))
+   :timestamp #'interpret-timestamp
+   :timestamp-with-timezone #'interpret-timestamp
+   :interval (lambda (months days usecs)
+               (make-instance 'interval :months months
+                              :ms (floor (+ (* days +apomo-usecs-in-one-day+) usecs) 1000)))))
+
+(defmethod cl-postgres-async:to-sql-string ((arg date))
+  (multiple-value-bind (year month day) (decode-date arg)
+    (values (format nil "~4,'0d-~2,'0d-~2,'0d" year month day) "date")))
+
+(defmethod cl-postgres-async:to-sql-string ((arg timestamp))
+  (multiple-value-bind (year month day hour min sec ms) (decode-timestamp arg)
+    (values
+     (format nil "~4,'0d-~2,'0d-~2,'0d ~2,'0d:~2,'0d:~2,'0d~@[.~3,'0d~]"
+             year month day hour min sec (if (zerop ms) nil ms))
+     "timestamp")))
+
+(defmethod cl-postgres-async:to-sql-string ((arg interval))
+  (multiple-value-bind (year month day hour min sec ms) (decode-interval arg)
+    (if (= year month day hour min sec ms 0)
+        (values "0 milliseconds" "interval")
+        (flet ((not-zero (x) (if (zerop x) nil x)))
+          (values
+           (format nil "~@[~d years ~]~@[~d months ~]~@[~d days ~]~@[~d hours ~]~@[~d minutes ~]~@[~d seconds ~]~@[~d milliseconds~]"
+                   (not-zero year) (not-zero month) (not-zero day)
+                   (not-zero hour) (not-zero min) (not-zero sec) (not-zero ms))
+           "interval")))))
